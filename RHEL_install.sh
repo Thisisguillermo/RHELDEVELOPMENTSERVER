97640a6de378890c3c9078520135fbac76654f4b
Guillermo <guillermozaandam@gmail.com>
2020-05-24 18:22:20 +0200

    ...

--- RHEL_install.sh
+++ RHEL_install.sh
@@ -36,7 +36,29 @@ sudo dnf install -y vim
 sudo dnf install -y podman-docker 
 sudo dnf install -y ansible
 sudo dnf install -y gnome-tweaks
-sudp dnf install -y spice-vdagent
+sudo dnf install -y spice-vdagent
+
+# Lynis is one of the most trusted automated auditing tool for software patch management, malware scanning and vulnerability detecting in Unix/Linux based systems. (https://cisofy.com/)
+sudo dnf install -y lynis
+
+# Auditing with Auditd Tool on CentOS/RHEL https://www.tecmint.com/linux-system-auditing-with-auditd-tool-on-centos-rhel/
+sudo dnf install -y auditd
+
+# Autofs for automatically mounting directories at request basis instead of automatically when the server starts.
+# "Autofs uses the automount daemon to manage your mount points by only mounting them dynamically when they are accessed."
+sudo dnf install -y autofs
+
+# SeLinux Troubleshoot program
+sudo dnf install -y setroubleshoot
+
+# SELinux Policy Management tool.
+sudo dnf install -y policycoreutils-python
+
+# Program to configure kickstart files
+sudo dnf install -y system-config-kickstart
+
+# Verifying the Kickstart File woth pykickstart
+sudo dnf install -y pykickstart
 
 # GUI installation.
 #sudo dnf groupinstall -y "GNOME Desktop" --skip-broken
@@ -54,7 +76,6 @@ sudp dnf install -y spice-vdagent
 
 # Google Performance Tools
 # Is a collection of tools for analyzing and improving performance of multi-threaded applications. It offers a fast malloc, a thread-friendly heap-checker, a heap-profiler, and a cpu-profiler.
-
 sudo dnf install -y perf
 sudo dnf install -y gperftools
 
